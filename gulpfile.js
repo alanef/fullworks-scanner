139f473955ad3c51f741d56fa8ae97f909c487fc
alanef <alan@fullworks.net>
2023-04-03 17:03:20 +0100

    v 1.0.0  plugin submission - tweakgulp build and translation file

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,30 @@
-var project                 = 'fullworks-vulnrability-scanner'; // Project Name.
-// @TODO remove non dist from vendor  see widget-for-eventbrite-api for how
-// @TODO add james kemps freemius upload gulp
+var project = 'fullwork-svulnerability-scanner'; // Project Name.
 
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 var del = require('del');
 var rename = require('gulp-rename');
 var gutil = require('gulp-util');
-var dirSync = require( 'gulp-directory-sync' );
+var dirSync = require('gulp-directory-sync');
 var removeLines = require('gulp-remove-lines');
 var wpPot = require('gulp-wp-pot');
 var sort = require('gulp-sort');
 var notify = require("gulp-notify");
 
-gulp.task('build', ['remove:secret'], function() {
+
+gulp.task('zip', (done) => {
     gulp.src('dist/**/*')
-        .pipe(rename(function(file) {
-            file.dirname = project+'/' + file.dirname;
+        .pipe(rename(function (file) {
+            file.dirname = project + '/' + file.dirname;
         }))
-        .pipe(zip(project+'.zip'))
+        .pipe(zip(project + '-free.zip'))
         .pipe(gulp.dest('zipped'))
+    done()
 });
 
-gulp.task('remove:secret',['clean:files','translate'], function () {
-    gulp.src('src/includes/class-freemius-config.php')
-        .pipe(removeLines({
-            'filters': [
-                /Set the SDK/,
-                /IMPORTANT/,
-                /\'secret_key+/
-            ]
-        }))
-        .pipe(gulp.dest('dist/includes'));
-});
 
-gulp.task('clean:files',['sync'], function () {
+gulp.task('clean', () => {
     return del([
-        'dist/**/sass/',
-        'dist/**/*.css.map',
         'dist/composer.*',
         'dist/includes/vendor/bin/',
         'dist/includes/vendor/composer/ca-bundle/',
@@ -49,63 +36,32 @@ gulp.task('clean:files',['sync'], function () {
         'dist/includes/vendor/**/package.json',
         'dist/includes/vendor/**/gulpfile.js',
         'dist/includes/vendor/**/*.md',
-        'dist/includes/vendor/maxmind/',
-        'dist/includes/vendor/prospress/action-scheduler/docs/',
-        'dist/includes/vendor/prospress/action-scheduler/tests/',
-        'dist/includes/vendor/faisalman/ua-parser-js/src/',
-        'dist/includes/vendor/faisalman/ua-parser-js/test/',
-        'dist/includes/vendor/faisalman/ua-parser-js/.npmrc',
-        'dist/includes/vendor/faisalman/ua-parser-js/bower.json',
-        'dist/includes/vendor/faisalman/ua-parser-js/package.js',
-        'dist/includes/vendor/running-coder/jquery-typeahead/src/',
-        'dist/includes/vendor/running-coder/jquery-typeahead/example/',
-        'dist/includes/vendor/running-coder/jquery-typeahead/src/',
-        'dist/includes/vendor/running-coder/jquery-typeahead/test/',
-        'dist/includes/vendor/running-coder/jquery-typeahead/.babelrc',
-        'dist/includes/vendor/running-coder/jquery-typeahead/.editorconfig',
-        'dist/includes/vendor/running-coder/jquery-typeahead/bower.json',
-        'dist/includes/vendor/running-coder/jquery-typeahead/gulpfile.babel.js',
-        'dist/includes/vendor/running-coder/jquery-typeahead/yarn.lock',
-        'dist/includes/vendor/tomverran/robots-txt-checker/tests/',
-        'dist/includes/vendor/tomverran/robots-txt-checker/phpunit.xml',
-        'dist/includes/vendor/tomverran/robots-txt-checker/Vagrantfile',
-        'dist/includes/vendor/Valve/fingerprintjs2/flash/',
-        'dist/includes/vendor/Valve/fingerprintjs2/specs/',
-        'dist/includes/vendor/Valve/fingerprintjs2/.eslintrc',
-        'dist/includes/vendor/Valve/fingerprintjs2/bower.json',
-        'dist/includes/vendor/Valve/fingerprintjs2/fingerprint2.js',
-        'dist/includes/vendor/Valve/fingerprintjs2/index.html',
-        'dist/includes/vendor/Valve/fingerprintjs2/yarn.lock'
     ]);
 });
 
 
-gulp.task( 'sync', function() {
-    return gulp.src( '' )
-        .pipe(dirSync( 'src', 'dist', { printSummary: true } ))
+gulp.task('sync', () => {
+    return gulp.src('.', {allowEmpty: true})
+        .pipe(dirSync('src', 'dist', {printSummary: true}))
         .on('error', gutil.log);
-} );
+});
 
-gulp.task( 'translate', function () {
-    return gulp.src( ['src/**/*.php','!src/includes/{vendor,vendor/**}'])
+gulp.task('translate', () => {
+    return gulp.src(['src/**/*.php', '!src/includes/{vendor,vendor/**}'])
         .pipe(sort())
-        .pipe(wpPot( {
-            domain        : project,
-            package       : project
-        } ))
+        .pipe(wpPot({
+            domain: project,
+            package: project
+        }))
         .on('error', gutil.log)
-        .pipe(gulp.dest('src/languages/'+project+'.pot'))
-        .pipe(gulp.dest('dist/languages/'+project+'.pot'))
-        .pipe( notify( { message: 'TASK: "translate" Completed! 💯', onLast: true } ) );
+        .pipe(gulp.dest('src/languages/' + project + '.pot'))
+        .pipe(gulp.dest('dist/languages/' + project + '.pot'))
+        .pipe(notify({message: 'TASK: "translate" Completed! 💯', onLast: true}));
 
 });
-var livereload = require('gulp-livereload');
 
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('src/**/*.*',['reload']);
-});
-gulp.task('reload', function() {
-        livereload();
-        console.log('changed !');
-});
+
+gulp.task('build', gulp.series('sync',  'clean', 'translate', 'zip'));
+
+
+
